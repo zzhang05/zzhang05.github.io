e353191d1ec6386cd41656e10e7b157138d990e6
zzhang05 <93588008+zzhang05@users.noreply.github.com>
2026-01-05 13:31:10 +0000

    Deploying to gh-pages from @ zzhang05/zzhang05.github.io@eec01ea6602fa1bfc126413665788e9c536b00b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/patents/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
