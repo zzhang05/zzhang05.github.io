cdf1589052133fdf3c1f031f0e658c42da8dffb0
zzhang05 <93588008+zzhang05@users.noreply.github.com>
2026-01-31 14:55:36 +0000

    Deploying to gh-pages from @ zzhang05/zzhang05.github.io@d9be63de227e07fc25a67e2c673351caf52cfe2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,8 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-one-paper-has-been-accepted-by-ches2025",
-          title: 'One Paper has been accepted by CHES2025',
+            },},{id: "news-one-paper-has-been-accepted-by-sigir2025",
+          title: 'One Paper has been accepted by SIGIR2025',
           description: "",
           section: "News",},{id: "news-one-paper-has-been-accepted-by-icml2025",
           title: 'One Paper has been accepted by ICML2025',
