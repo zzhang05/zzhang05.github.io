e3166d63be335e996ab2c1bfcdc250f38207df25
zzhang05 <93588008+zzhang05@users.noreply.github.com>
2026-01-31 14:11:53 +0000

    Deploying to gh-pages from @ zzhang05/zzhang05.github.io@957056dab0311fa98e5ea90be16eb430e74964d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Most of my papers listed below are published in top-tier international conferences and journals in the field of computer vision, machine learning, artificial intelligence and multimedia, which are very selective with an acceptance rate in general below 25%.",
+          description: "Most of my papers listed below are published in top-tier international conferences and journals in the field of computer vision, machine learning, artificial intelligence and multimedia, which are very selective with an acceptance rate in general below 25%. As the first author, I have published 1 ICML, 1 CVPR, 1 SIGGRAPH Asia, 2 ACM MM, 1, SIGIR, 2 TMM, 2 WACV, 1 CIKM, 1 ICASSP and 1 ICIP.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
