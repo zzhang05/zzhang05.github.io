7ce08b53f43fee0c07540422e051e062c02d8380
zzhang05 <93588008+zzhang05@users.noreply.github.com>
2026-08-27 02:46:16 +0000

    Deploying to gh-pages from @ zzhang05/zzhang05.github.io@2e29c60089290a10d6bf1bdc05af396f2bd2a8ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-patents-and-competition",
-          title: "Patents and Competition",
+        },{id: "nav-patents",
+          title: "Patents",
           description: "These granted patents played important roles for the Huawei’s new product line including all series Phones.",
           section: "Navigation",
           handler: () => {
